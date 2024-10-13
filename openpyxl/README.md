385a0c3e95e47cf253725336c6bdd30c7b1d3a59
むずでょ <muzudho1@gmail.com>
2024-10-13 20:57:26 +0900

    font

--- openpyxl/README.md
+++ openpyxl/README.md
@@ -3,6 +3,7 @@
 * 📖 [PythonでExcelファイルを作成，書き込み](https://qiita.com/si-tora/items/439cbce030ba962ea815)
 * 📖 [openpyxlでセルに色を塗る](https://qiita.com/github-nakasho/items/3f861395227e5645cce7)
 * 📖 [Excels 56 ColorIndex Colors](https://www.excelsupersite.com/what-are-the-56-colorindex-colors-in-excel/)
+* 📖 [Pythonでエクセルファイルの文字色を赤字にする方法](https://utataneblog760.com/016moji/)
 
 # 参考記事　＞　エクセル関連
 
--- openpyxl/let_s_make_excel_file.py
+++ openpyxl/let_s_make_excel_file.py
@@ -8,10 +8,11 @@ import traceback
 import time
 import datetime
 import openpyxl as xl
-from openpyxl.styles import PatternFill
+from openpyxl.styles import PatternFill, Font
 
 
 FILE_NAME = 'hello.xlsx'
+MSG_SECS = 0.1     # メッセージ送りの長さ（秒）
 
 
 ########################################
@@ -19,17 +20,21 @@ FILE_NAME = 'hello.xlsx'
 ########################################
 if __name__ == '__main__':
     try:
+        ########################
+        # MARK: ワークブック作成
+        ########################
+
         print()             # 空行
-        time.sleep(0.2)     # メッセージが少しずつ進むようにウェイトを入れます
+        time.sleep(MSG_SECS)     # メッセージが少しずつ進むようにウェイトを入れます
         
         print("空のシートを１つ持つエクセルファイルを新規作成しましょう。")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print("ファイル名は hello.xlsx とします。")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print("ファイルが既存の場合は上書きします")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         enter_key = input(f"""\
 それでは、エンター・キーを打鍵してみてください
@@ -38,46 +43,56 @@ if __name__ == '__main__':
         wb = xl.Workbook()
         wb.save(FILE_NAME)  # 既存のファイルがあれば上書きになる
 
-        print()
-        time.sleep(0.2)
-
         print(f"{FILE_NAME} ファイルを作成しました")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"{FILE_NAME} ファイルを開けてみてください。")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
-        print(f"デフォルトで Sheet という名前のシートがあります。")
-        time.sleep(0.2)
+        print(f"デフォルトで Sheet という名前のシートがありますか？")
+        time.sleep(MSG_SECS)
+
+
+        ####################
+        # MARK: シート名変更
+        ####################
+
+        print()
+        time.sleep(MSG_SECS)
 
         print("この Sheet シートの名前を Hello に変えましょう")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"それでは、{FILE_NAME} ファイルを閉じ、")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         enter_key = input("エンター・キーを打鍵してみてください")
 
         wb['Sheet'].title = 'Hello'
         wb.save(FILE_NAME)
 
-        print()
-        time.sleep(0.2)
-
         print(f"Sheet シートの名前を Hellow に変えて {FILE_NAME} ファイルを保存しました")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"{FILE_NAME} ファイルを開けてみてください。")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
-        print("Hello という名前のシートがあります。")
-        time.sleep(0.2)
+        print("Hello という名前のシートがありますか？")
+        time.sleep(MSG_SECS)
+
+
+        ################
+        # MARK: 文字入力
+        ################
+
+        print()
+        time.sleep(MSG_SECS)
 
         print("この Sheet シートの A1 セルに World と文字を入れてみましょう")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"それでは、{FILE_NAME} ファイルを閉じ、")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         enter_key = input("エンター・キーを打鍵してみてください")
 
@@ -86,24 +101,74 @@ if __name__ == '__main__':
         wb.save(FILE_NAME)
 
         print()
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"Hello シートの A1 セルに World と入力して {FILE_NAME} ファイルを保存しました")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"{FILE_NAME} ファイルを開けて確認してみてください。")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
+
+        print(f"Hello シートの A1 セルに World と入力されていますか？")
+        time.sleep(MSG_SECS)
+
+
+        ##############
+        # MARK: 背景色
+        ##############
+
+        print()
+        time.sleep(MSG_SECS)
 
         print("次は、 A2 セルの背景に色を付けてみましょう")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         print(f"それでは、{FILE_NAME} ファイルを閉じ、")
-        time.sleep(0.2)
+        time.sleep(MSG_SECS)
 
         enter_key = input("エンター・キーを打鍵してみてください")
 
         sheet['A2'].fill = PatternFill(patternType='solid', fgColor='CCFFFF')
         wb.save(FILE_NAME)
+
+        print(f"Hello シートの A2 セルの背景に色を付けて {FILE_NAME} ファイルを保存しました")
+        time.sleep(MSG_SECS)
+
+        print(f"{FILE_NAME} ファイルを開けて確認してみてください。")
+        time.sleep(MSG_SECS)
+
+        print(f"Hello シートの A2 セルの背景に色が付いていますか？")
+        time.sleep(MSG_SECS)
+
+
+        ##################
+        # MARK: フォント色
+        ##################
+
+        print()
+        time.sleep(MSG_SECS)
+
+        print("次は、 A3 セルに Apple という文字を入力して赤色にしてみましょう")
+        time.sleep(MSG_SECS)
+
+        print(f"それでは、{FILE_NAME} ファイルを閉じ、")
+        time.sleep(MSG_SECS)
+
+        enter_key = input("エンター・キーを打鍵してみてください")
+
+        sheet['A3'].value = 'Apple'
+        sheet['A3'].font = Font(color='FF0000')
+        wb.save(FILE_NAME)
+
+        print(f"Hello シートの A3 セルに Applet という文字を入力して赤色にし、 {FILE_NAME} ファイルを保存しました")
+        time.sleep(MSG_SECS)
+
+        print(f"{FILE_NAME} ファイルを開けて確認してみてください。")
+        time.sleep(MSG_SECS)
+
+        print(f"Hello シートの A2 セルの背景に色が付いていますか？")
+        time.sleep(MSG_SECS)
+
 
         print("おわり。")
 
